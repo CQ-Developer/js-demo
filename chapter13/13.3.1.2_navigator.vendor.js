6d72a5f67182325c967b64703e9c8fb5074ce259
qchen71 <qchen71@ford.com>
2022-07-28 14:51:40 +0800

    navigator.vendor

---
+++ chapter13/13.3.1.2_navigator.vendor.js
@@ -0,0 +1,10 @@
+/**
+ * 13.3.1.2 navigator.vendor
+ * 
+ * navigator.vendor属性是一个字符串, 通常包含浏览器开发商信息.
+ * 返回这个字符串是浏览器navigator兼容模式的一个功能.
+ * 
+ * 根据HTML实时标准:
+ * navigator.vendor返回一个空字符串, 也可能返回字符串"Apple Computer, Inc"或"Google Inc"
+ */
+console.log(navigator.vendor);
