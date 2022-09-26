6f0ed722a75e3105da9e36e7fdccd18643b400df
Chen <18551681083@163.com>
2022-09-26 22:07:19 +0800

    激活中状态

--- chapter27-WorkerThread/27.4_ServiceWorkerGlobalScope/5_lifecycle/serviceWorker.js
+++ chapter27-WorkerThread/27.4_ServiceWorkerGlobalScope/5_lifecycle/serviceWorker.js
@@ -7,3 +7,12 @@ self.addEventListener("install", event => {
     // 如果没有错误发生或者没有reject, 服务工作者线程就会前进到installed状态
     event.waitUntil(caches.open("v1").then((cache) => cache.addAll(["foo.js", "bar.html", "baz.css"])));
 });
+
+// 4.激活中状态
+self.addEventListener("activate", (event) => {
+    console.log("Service worker is in the activating state");
+    // 清除所有版本比较老的缓存
+    caches.keys()
+        .then((keys) => keys.filter((key) => key != "v3"))
+        .then((oldKeys) => oldKeys.forEach((oldKey) => caches.delete(oldKey)));
+});
